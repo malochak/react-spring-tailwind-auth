98dc8759969c4ccecf98e931b09e49e0a4954b68
malochak <konrad_malocha@hotmail.com>
2023-08-10 21:33:00 +0200

    implement raw login form

--- web-client/src/screens/Login.js
+++ web-client/src/screens/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from "../components/layout/Layout";
+import LoginForm from "../components/forms/LoginForm";
 
 function Login() {
     return (
         <Layout>
-            <div>Login</div>
+            <h1 className="text-2xl font-bold mb-3">Sign In</h1>
+            <LoginForm />
         </Layout>
     );
 }
