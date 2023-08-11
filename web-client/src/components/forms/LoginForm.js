37cf0ac4df9ed12d796d8ee9c0d362c5956dac56
malochak <konrad_malocha@hotmail.com>
2023-08-11 20:07:52 +0200

    add handling state of login form using react-hook-form

--- web-client/src/components/forms/LoginForm.js
+++ web-client/src/components/forms/LoginForm.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
 
-function LoginForm(props) {
+function LoginForm() {
+
+    const {
+        register,
+        handleSubmit,
+    } = useForm()
+
+    // todo implement api call to authenticate with given credentials
+    const onSubmit = (data) => console.log(data)
+
     return (
-        <form className="flex flex-col">
-            <input type="text" placeholder="E-mail" className="input input-bordered input-primary w-full max-w-xs"/>
-            <input type="password" placeholder="Password"
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+            <input type="email"
+                   placeholder="E-mail"
+                   className="input input-bordered input-primary w-full max-w-xs"
+                   {...register("email", { required: true })}
+            />
+            <input type="password"
+                   placeholder="Password"
+                     {...register("password", { required: true })}
                    className="input input-bordered input-primary w-full max-w-xs"/>
             <button className="btn btn-primary w-fit ">Sign In</button>
         </form>
