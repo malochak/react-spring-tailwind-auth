ce84913790f5f3842faae9d8b7722e1d9def636e
malochak <konrad_malocha@hotmail.com>
2023-08-16 15:48:51 +0200

    add google button to sign in/up

--- web-client/src/screens/Authentication.js
+++ web-client/src/screens/Authentication.js
@@ -8,6 +8,7 @@ export const AuthenticationType = {
     LOGIN: "login",
     REGISTER: "register"
 }
+
 function Authentication(props) {
 
     const [title, setTitle] = useState("Sign in to your account")
@@ -19,18 +20,27 @@ function Authentication(props) {
     return (
         <Layout>
             <div className="flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8">
-
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-
                     <img
                         className="mx-auto h-14 w-auto"
                         src={logo}
                         alt="App Name logo"
                     />
                     <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        { title }
+                        {title}
                     </h2>
-                    {props.type === AuthenticationType.LOGIN ? (<LoginForm />) : (<RegisterForm />)}
+                    {props.type === AuthenticationType.LOGIN ? (<LoginForm/>) : (<RegisterForm/>)}
+
+                    <div className="my-8 divider"> Or continue with</div>
+
+
+                    <button
+                        className="px-4 py-2 border w-full flex gap-2 rounded-lg hover:border-slate-400 hover:shadow transition duration-300">
+                        <img className="w-6 h-6 justify-start"
+                             src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy"
+                             alt="google logo"/>
+                        <span className="justify-center">Google</span>
+                    </button>
                 </div>
             </div>
         </Layout>
