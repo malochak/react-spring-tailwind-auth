9f8fa09650c3817c2b8c90bef92a88c5567c0f49
malochak <konrad_malocha@hotmail.com>
2023-08-17 21:19:58 +0200

    add in memory User Service

---
+++ server/src/main/java/dev/mkon/server/api/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package dev.mkon.server.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+            // todo get rid of default password encoder!!!
+            User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+}
