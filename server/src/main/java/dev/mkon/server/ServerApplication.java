dda7dcfa7eb55880f284500b9582f3adc7f3b341
malochak <konrad_malocha@hotmail.com>
2023-08-22 19:16:27 +0200

    fix dummy test with context loading - CommandLineRunner has been removed, as it tries to put user into db, while db is not available (might be profiled, but dummy data for tests will be handled in different way)

--- server/src/main/java/dev/mkon/server/ServerApplication.java
+++ server/src/main/java/dev/mkon/server/ServerApplication.java
@@ -2,31 +2,12 @@ package dev.mkon.server;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import dev.mkon.server.api.security.model.user.DefaultUserDetailsService;
 
 @Slf4j
 @SpringBootApplication
 public class ServerApplication {
-
-    private final DefaultUserDetailsService userDetailsService;
-
-    ServerApplication(DefaultUserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return args -> {
-            var response = userDetailsService.save("konrad@mkon.dev", "test");
-
-            log.info("response: [{}] {}", response.getStatusCode(), response.getBody());
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
