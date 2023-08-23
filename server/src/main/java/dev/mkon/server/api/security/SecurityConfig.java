65ab4ca4c642e7823e510475fb9094334f6b493b
malochak <konrad_malocha@hotmail.com>
2023-08-23 21:28:48 +0200

    replace dummy PasswordEncoder with BCryptPasswordEncoder

--- server/src/main/java/dev/mkon/server/api/security/SecurityConfig.java
+++ server/src/main/java/dev/mkon/server/api/security/SecurityConfig.java
@@ -25,17 +25,7 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return rawPassword.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return rawPassword.toString().equals(encodedPassword);
-            }
-        };
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
