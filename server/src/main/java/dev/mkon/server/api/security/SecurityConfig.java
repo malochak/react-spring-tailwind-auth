15414c3b2a457cf867576daf8a02591e698eae3e
malochak <konrad_malocha@hotmail.com>
2023-08-25 20:36:01 +0200

    correct ant matchers in SecurityConfig

--- server/src/main/java/dev/mkon/server/api/security/SecurityConfig.java
+++ server/src/main/java/dev/mkon/server/api/security/SecurityConfig.java
@@ -4,17 +4,11 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 @EnableWebSecurity
@@ -29,8 +23,9 @@ public class SecurityConfig {
             .cors(AbstractHttpConfigurer::disable)
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(requests -> requests
-                .anyRequest()
-                .permitAll())
+                .requestMatchers("/auth/**")
+                .permitAll()
+                .anyRequest().authenticated())
             .userDetailsService(userDetailsService);
 
         return http.build();
